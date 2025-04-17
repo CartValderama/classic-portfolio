aa00d74698b93dcf8075e62b5c3fd31a3703f9cd
Cart Valderama <cartantonio2000@gmail.com>
2025-04-17 05:02:13 +0200

    clean up code

--- src/components/Credits.tsx
+++ src/components/Credits.tsx
@@ -1,34 +1,8 @@
 import { useState } from "react";
+import { credits } from "../data/credits";
 
 const Credits = () => {
   const [currentCredit, setCurrentCredit] = useState(0);
-
-  const credits = [
-    {
-      task: "Credit",
-      people: ["Contributors in this project"],
-    },
-    {
-      task: "Developer",
-      people: ["Cart Valderama"],
-    },
-    {
-      task: "Designer",
-      people: ["Cart Valderama"],
-    },
-    {
-      task: "Monitor Design",
-      people: ["Jhon Valderama"],
-    },
-    {
-      task: "Critique",
-      people: ["Maryle Valderama", "Jhon Valderama", "Elijah Cuady"],
-    },
-    {
-      task: "Inspiration",
-      people: ["Windows 95 operating system"],
-    },
-  ];
 
   const handleClick = () => {
     setCurrentCredit((prev) => (prev + 1) % credits.length);
---
+++ src/data/credits.ts
@@ -0,0 +1,26 @@
+export const credits = [
+  {
+    task: "Credit",
+    people: ["Contributors in this project"],
+  },
+  {
+    task: "Developer",
+    people: ["Cart Valderama"],
+  },
+  {
+    task: "Designer",
+    people: ["Cart Valderama"],
+  },
+  {
+    task: "Monitor Design",
+    people: ["Jhon Valderama"],
+  },
+  {
+    task: "Critique",
+    people: ["Maryle Valderama", "Jhon Valderama", "Elijah Cuady"],
+  },
+  {
+    task: "Inspiration",
+    people: ["Windows 95 operating system"],
+  },
+];
