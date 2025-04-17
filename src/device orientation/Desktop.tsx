5754b3e6e276f4154b90aed7c55eb75693431b92
Cart Valderama <cartantonio2000@gmail.com>
2025-04-17 23:15:57 +0200

    fix animation speed and timing

--- src/device orientation/Desktop.tsx
+++ src/device orientation/Desktop.tsx
@@ -13,7 +13,7 @@ const Desktop = () => {
       <motion.div
         initial={{ x: 0, opacity: 1 }}
         animate={
-          start ? { x: -1600, opacity: 0, scale: 2 } : { opacity: 1, y: 0 }
+          start ? { x: -2000, opacity: 0, scale: 2 } : { opacity: 1, y: 0 }
         }
         transition={{ duration: 0.5, delay: start ? 0 : 1 }}
         layout="position"
